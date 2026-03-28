2a687793a02ef84777d1bd3bfa520cfc6adbe710
Shubham Kumar <shubham.kr2951@gmail.com>
2026-03-28 23:00:55 +0530

    Password Input: Password input component added

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
+import PasswordInput from "../../components/Input/PasswordInput";
 
 const Login = () => {
   return (
@@ -7,11 +8,13 @@ const Login = () => {
       <Navbar />
 
       <div className="flex items-center justify-center mt-28">
-        <div className="w-96 border border-gray-300 rounded-2xl bg-ghost p-6">
+        <div className="w-96 border-2 border-gray-200 rounded-2xl bg-ghost p-6">
           <form onSubmit={() => {}}>
             <h3 className="text-2xl mb-7">Login</h3>
 
             <input type="text" placeholder="Email" className="input-box" />
+
+            <PasswordInput />
 
             <button type="submit" className="btn-primary">
               Login
@@ -21,7 +24,7 @@ const Login = () => {
               Not registered yet?{" "}
               <Link
                 to="/sign-up"
-                className="font-medium text-accent underline cursor-pointer"
+                className="font-medium text-accent underline hover:decoration-dashed cursor-pointer"
               >
                 Create an Account
               </Link>
