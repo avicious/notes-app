6311ff5392443546779aae61a2d05c5472c0d706
Shubham Kumar <shubham.kr2951@gmail.com>
2026-03-28 23:27:32 +0530

    Helper Added: Validate email added as helper function

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,20 +1,56 @@
 import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import PasswordInput from "../../components/Input/PasswordInput";
+import { useState } from "react";
+import { validateEmail } from "../../utils/helper";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    setError("");
+
+    //Login
+  };
+
   return (
     <>
       <Navbar />
 
       <div className="flex items-center justify-center mt-28">
         <div className="w-96 border-2 border-gray-200 rounded-2xl bg-ghost p-6">
-          <form onSubmit={() => {}}>
+          <form onSubmit={handleLogin}>
             <h3 className="text-2xl mb-7">Login</h3>
 
-            <input type="text" placeholder="Email" className="input-box" />
+            <input
+              type="email"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
 
-            <PasswordInput />
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
 
             <button type="submit" className="btn-primary">
               Login
