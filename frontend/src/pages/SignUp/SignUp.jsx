5bacc9d1ee76a47ad339dda3e42e8334cdf7bc3d
Shubham Kumar <shubham.kr2951@gmail.com>
2026-03-28 23:43:27 +0530

    Sign Up Added: Added jsx to sign up page

--- frontend/src/pages/SignUp/SignUp.jsx
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,90 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+import PasswordInput from "../../components/Input/PasswordInput";
+import { validateEmail } from "../../utils/helper";
+
 const SignUp = () => {
-    return (
-        <div>SignUp</div>
-    );
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    if (!username) {
+      setError("Please enter a username.");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter a valid password.");
+      return;
+    }
+
+    setError("");
+
+    //Signup
+  };
+
+  return (
+    <>
+      <Navbar />
+
+      <div className="flex items-center justify-center mt-20">
+        <div className="w-96 border-2 border-gray-200 rounded-2xl bg-ghost p-6">
+          <form onSubmit={handleSignUp}>
+            <h3 className="text-2xl mb-7">Sign Up</h3>
+
+            <input
+              type="text"
+              placeholder="Username"
+              className="input-box"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+
+            <input
+              type="email"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+
+            <button type="submit" className="btn-primary">
+              Create Account
+            </button>
+
+            <p className="text-sm text-center mt-4">
+              Already have an account?{" "}
+              <Link
+                to="/login"
+                className="font-medium text-accent underline hover:decoration-dashed cursor-pointer"
+              >
+                Login
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SignUp;
