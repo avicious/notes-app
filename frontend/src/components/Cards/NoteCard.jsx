81af022c7918f387b8f81e4c90228fa2f4995ca0
Shubham Kumar <shubham.kr2951@gmail.com>
2026-03-30 01:31:32 +0530

    NoteCard Added: JSX added to NoteCard component

---
+++ frontend/src/components/Cards/NoteCard.jsx
@@ -0,0 +1,47 @@
+import { Pin, SquarePen, Trash } from "lucide-react";
+
+const NoteCard = ({
+  title,
+  date,
+  content,
+  tags,
+  isPinned,
+  onEdit,
+  onDelete,
+  onPinNote,
+}) => {
+  return (
+    <div className="border border-gray-200 rounded p-4 bg-ghost shadow-sm transition-all duration-300 ease-in-out hover:-translate-y-1.5 hover:shadow-xl hover:scale-[1.02]">
+      <div className="flex items-center justify-between">
+        <div>
+          <h3 className="text-sm font-medium">{title}</h3>
+          <span className="text-xs text-slate-500">{date}</span>
+        </div>
+
+        <Pin
+          className={`cursor-pointer ${isPinned ? "text-accent" : "text-slate-300"}`}
+          onClick={onPinNote}
+        />
+      </div>
+
+      <p className="text-xs text-slate-600 mt-2">{content?.slice(0, 60)}</p>
+
+      <div className="flex items-center justify-between mt-2">
+        <div className="text-xs text-slate-500">{tags}</div>
+
+        <div className="flex items-center gap-2">
+          <SquarePen
+            className="text-slate-300 hover:text-green-600 cursor-pointer"
+            onClick={onEdit}
+          ></SquarePen>
+          <Trash
+            className="text-slate-300 hover:text-red-500 cursor-pointer"
+            onClick={onDelete}
+          ></Trash>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoteCard;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,25 @@
+import NoteCard from "../../components/Cards/NoteCard";
 import Navbar from "../../components/Navbar/Navbar";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+
+      <div className="app-container">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <NoteCard
+            title="Meeting on 7th April"
+            date="3rd April 2026"
+            content="Meeting on 7th April"
+            tags="#Meeting"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
+      </div>
     </>
   );
 };
