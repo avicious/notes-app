9ddfd8b6be6a1520156100262f5f53ba99d40bb3
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-02 00:49:19 +0530

    Add Note: Backend for add note added

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,10 @@ import cors from "cors";
 import "dotenv/config";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
-import User from "./models/User.js";
 import authenticateToken from "./utilities.js";
+
+import User from "./models/User.js";
+import Note from "./models/Note.js";
 
 mongoose.connect(process.env.MONGO_URI);
 
@@ -109,8 +111,40 @@ app.post("/login", async (req, res) => {
 
 // Add Note
 app.post("/add-note", authenticateToken, async (req, res) => {
-  
-})
+  const { title, content, tags } = req.body;
+  const user = req.user;
+
+  if (!title) {
+    return res.status(400).json({ error: true, message: "Title is required" });
+  }
+
+  if (!content) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Content is required" });
+  }
+
+  try {
+    const note = new Note({
+      title,
+      content,
+      tags: tags || [],
+      userId: user._id,
+    });
+
+    await note.save();
+
+    return res.json({
+      error: false,
+      note,
+      message: "Note added successfully",
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: true, message: "Internal Server Error" });
+  }
+});
 
 app.listen(8000);
 
