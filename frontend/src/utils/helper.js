ec0d6b1d1b483f315c74b18e6521108264101427
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-02 22:30:41 +0530

    validatePassword Added: ValidatePassword function added to the helpers

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -3,6 +3,41 @@ export const validateEmail = (email) => {
   return regex.test(email);
 };
 
+export const validatePassword = (password) => {
+  const errors = [];
+
+  if (!password) {
+    return { isValid: false, errors: ["Password cannot be empty."] };
+  }
+
+  if (password.length < 8) {
+    errors.push("Password must be at least 8 characters long.");
+  }
+
+  if (!/[A-Z]/.test(password)) {
+    errors.push("Password must contain at least one uppercase letter.");
+  }
+
+  if (!/[a-z]/.test(password)) {
+    errors.push("Password must contain at least one lowercase letter.");
+  }
+
+  if (!/\d/.test(password)) {
+    errors.push("Password must contain at least one number.");
+  }
+
+  if (!/[@$!%*?&]/.test(password)) {
+    errors.push(
+      "Password must contain at least one special character (@, $, !, %, *, ?, &).",
+    );
+  }
+
+  return {
+    isValid: errors.length === 0,
+    errors: errors,
+  };
+};
+
 export const getInitials = (name) => {
   if (!name) return "";
 
