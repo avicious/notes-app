944bb0dc3b9c612c5500d984a7dafd61c3a79f4a
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-03 00:06:09 +0530

    Compare Password: Method to compare password added

--- backend/models/User.js
+++ backend/models/User.js
@@ -31,5 +31,9 @@ userSchema.pre("save", async function (next) {
   }
 });
 
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
 const User = mongoose.model("User", userSchema);
 export default User;
