49d52e6174f9fbb3d14fcafa82319688e80e7caa
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-02 23:43:13 +0530

    AuthenticateToken Cleanup: Added status codes and cleaned up authenticateToken function

--- backend/utilities.js
+++ backend/utilities.js
@@ -5,11 +5,20 @@ function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) return res.sendStatus(401);
+  if (!token) {
+    return res
+      .status(401)
+      .json({ error: true, message: "Access token missing" });
+  }
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(401);
-    req.user = user;
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decodedPayload) => {
+    if (err) {
+      return res
+        .status(403)
+        .json({ error: true, message: "Invalid or expired token" });
+    }
+
+    req.user = decodedPayload;
     next();
   });
 }
