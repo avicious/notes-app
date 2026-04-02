5fa0c9e206341b84aa09e4dd09defb8bf014e0ea
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-02 23:01:29 +0530

    Bcrypt Added: Password hashed and salted in User schema using pre-save

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
@@ -14,4 +15,21 @@ const userSchema = new Schema({
   createdOn: { type: Date, default: Date.now },
 });
 
-export default mongoose.model("User", userSchema);
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password, salt);
+
+    user.password = hash;
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
