3b8ddba32bed8dab5116a82c7ef088e9003b185a
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-03 00:24:53 +0530

    Note Schema: Fixed and cleaned Note schema

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -2,13 +2,36 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const noteSchema = new Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  tags: { type: [String], default: [] },
-  isPinned: { type: Boolean, default: false },
-  userId: { type: String, required: true },
-  createdOn: { type: Date, default: Date.now },
-});
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    tags: {
+      type: [String],
+      default: [],
+    },
+    isPinned: {
+      type: Boolean,
+      default: false,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 export default mongoose.model("Note", noteSchema);
