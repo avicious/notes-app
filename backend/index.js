b8d7061c825e8e717223c52354ed64e7b3f66786
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-05 22:50:21 +0530

    Get Notes: Get all notes route added

--- backend/index.js
+++ backend/index.js
@@ -125,6 +125,28 @@ app.post("/auth/login", validate(loginSchema), async (req, res) => {
   }
 });
 
+// All Notes
+app.get("/notes", authenticateToken, async (req, res) => {
+  const { userId } = req.user;
+
+  console.log(userId);
+
+  try {
+    const notes = await Note.find({ userId }).sort({ isPinned: -1 });
+
+    return res.status(200).json({
+      error: false,
+      notes,
+      message: "All notes retrieved successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: "Internal Server Error",
+    });
+  }
+});
+
 // Add Note
 app.post(
   "/notes",
