58ca7a30b72585b841e720a74c9316c259136236
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-13 23:42:08 +0530

    EmptyCard Cleanup: Did some CSS cleanup

--- frontend/src/components/EmptyCard/EmptyCard.jsx
+++ frontend/src/components/EmptyCard/EmptyCard.jsx
@@ -3,14 +3,13 @@ import noNote from "../../assets/no-note.png";
 
 const EmptyCard = ({ isSearch }) => {
   return (
-    <div className="flex flex-col items-center justify-center mt-20">
-      <img src={isSearch ? noNote : addNote} alt="No Notes" className="w-60" />
+    <div className="flex flex-col items-center justify-center mt-40">
+      <img src={isSearch ? noNote : addNote} alt="No Notes" className="w-25" />
 
       <p className="w-1/2 text-sm font-medium text-slate-700 text-center leading-7 mt-5">
         {isSearch
           ? "No notes found matching your search."
-          : `Capture your thoughts, ideas, and reminders effortlessly. <br /> Click
-        the Add button to create your first note.`}
+          : `Click the Add button to create your first note.`}
       </p>
     </div>
   );
