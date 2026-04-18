96d21c9828610535fe785a3d0fd03d1f28891106
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-18 23:43:21 +0530

    Main: Comments added for better readability

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import Modal from "react-modal";
+
 import './index.css'
 import App from './App.jsx'
 
+// Adding model to the root
 Modal.setAppElement("#root");
 
 createRoot(document.getElementById('root')).render(
