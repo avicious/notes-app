17ce0d29558db9bb32dcac153bd74d2b135a16e4
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-19 23:32:13 +0530

    Routes Refactored: Cleaner approach for routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home, Login, SignUp, GetStarted } from "./pages";
 
-const routes = (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<GetStarted />} />
-      <Route path="/dashboard" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<SignUp />} />
-    </Routes>
-  </BrowserRouter>
-);
-
 const App = () => {
-  return <div>{routes}</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<GetStarted />} />
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
