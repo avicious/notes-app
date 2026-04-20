6b69ca148dd805dc84ca1ab13ced477f64b8f827
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-20 23:56:31 +0530

    Auth Context: Auth context added to verify user

---
+++ frontend/src/utils/AuthContext.jsx
@@ -0,0 +1,40 @@
+import { createContext, useState, useEffect, useContext } from "react";
+import axiosInstance from "./axiosInstance";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const verifyUser = async () => {
+      try {
+        const response = await axiosInstance.get("/get-user");
+
+        if (response.status === 200) {
+          setIsAuthenticated(true);
+          setUser(response.data.user);
+        }
+      } catch (error) {
+        setIsAuthenticated(false);
+        setUser(null);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    verifyUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ isAuthenticated, isLoading, user, setIsAuthenticated }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
