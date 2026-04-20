593e933fb6a811da33c541b669744ab9a2ab60e4
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-20 23:58:18 +0530

    Protected Route: Auth context added to verify user

--- frontend/src/utils/ProtectedRoute.jsx
+++ frontend/src/utils/ProtectedRoute.jsx
@@ -4,6 +4,10 @@ import { useAuth } from "./AuthContext";
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, isLoading } = useAuth();
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
   }
